fba93fae99463abbfbacb7b41dbf6a16ef68126b
gaagbaag <fDhMV6MJ3@LDz39>
2025-03-06 14:18:22 -0300

    Conf

---
+++ wp-content/themes/astra-child/functions.php
@@ -0,0 +1,12 @@
+<?php
+function child_theme_enqueue_styles() {
+    wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
+}
+add_action('wp_enqueue_scripts', 'child_theme_enqueue_styles');
+
+// SVG Files
+function cc_mime_types($mimes) {
+  $mimes['svg'] = 'image/svg+xml';
+  return $mimes;
+}
+add_filter('upload_mimes', 'cc_mime_types');
